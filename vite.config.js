50477a3d02390a754302dc900d5a437d07a05848
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2025-04-20 23:56:59 +0000

    fix: Update base URL in Vite configuration to point to the correct path

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: 'https://joacohbc.github.io/'
+  base: 'https://joacohbc.github.io/study-builder'
 })
