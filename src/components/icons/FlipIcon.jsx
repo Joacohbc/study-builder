b09a79c268ccddb801e60cc4e857a66b032d8022
Joacohbc <69821825+Joacohbc@users.noreply.github.com>
2025-05-08 03:04:39 +0000

    feat: Update FlipIcon component with new SVG design for improved visual representation

--- src/components/icons/FlipIcon.jsx
+++ src/components/icons/FlipIcon.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
 const FlipIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <path d="M17 2.1l-10 10L17 22"></path>
-    <path d="M7 12h14"></path>
-  </svg>
+<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <path d="M12 16h4v4" />
+  <path d="M19.458 11.042c.86 -2.366 .722 -4.58 -.6 -5.9c-2.272 -2.274 -7.185 -1.045 -10.973 2.743c-3.788 3.788 -5.017 8.701 -2.744 10.974c2.227 2.226 6.987 1.093 10.74 -2.515" />
+</svg>
 );
 
 export default FlipIcon;
